01098bb3371d631af125767406fcff562fae99e2
Eduardo Reynaga Cobos <eduardo.reynagacobos@unidemat.edu.mx>
2025-02-13 10:59:47 -0600

    Add files via upload

--- 3.Area_y_Perimetro/Area_y_Perimetro/Form1.cs
+++ 3.Area_y_Perimetro/Area_y_Perimetro/Form1.cs
@@ -2,6 +2,8 @@ namespace Area_y_Perimetro
 {
     public partial class Form1 : Form
     {
+        /*utilizamos una declaracion publica para poder llamar el
+        variable Resulado en cualquier momento*/
         public double Resultado;
 
         public Form1()
@@ -11,6 +13,9 @@ namespace Area_y_Perimetro
 
         private void btnArea_Click(object sender, EventArgs e)
         {
+            /*declaramos como variable el radio y el area del circulo y 
+            ingresamos la libreria Math.PI para utilizar 3.14*/
+
             double radio = Convert.ToDouble(txtRadio.Text);
             double area = (Math.PI * radio);
             lblResultado.Text = "Area: " + area;
@@ -19,11 +24,14 @@ namespace Area_y_Perimetro
 
         private void btnPerimetro_Click(object sender, EventArgs e)
         {
+            /*declaramos como variable el radio y el perimetro del circulo y 
+            ingresamos la libreria Math.PI para utilizar 3.14*/
             double radio = Convert.ToDouble(txtRadio.Text);
             double perimetro = (2 * Math.PI * radio);
             lblResultado.Text = "Perimetro: " + perimetro;
         }
 
+       //una foto usando la harramienta de picturebox
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
--- 3.Area_y_Perimetro/Area_y_Perimetro/obj/Debug/net8.0-windows/Area_y_Perimetro.AssemblyInfo.cs
+++ 3.Area_y_Perimetro/Area_y_Perimetro/obj/Debug/net8.0-windows/Area_y_Perimetro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Area_y_Perimetro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e367c194f4392478d552374369baec6b75d0bf46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Area_y_Perimetro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Area_y_Perimetro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
